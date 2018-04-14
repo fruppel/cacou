aa3b5429daefdb2c95af23fbd39c548cdd4d8522
fruppel <skywalker9947>
2018-04-14 13:17:50 +0200

    Show graph data

---
+++ resources/assets/js/components/WeightChart.vue
@@ -0,0 +1,34 @@
+<template>
+    <canvas id="weight-chart" height="75"></canvas>
+</template>
+
+<script>
+    import Chart from 'chart.js';
+
+    export default {
+
+        props: ['labels', 'values'],
+
+        mounted() {
+
+            const canvas = document.querySelector('#weight-chart').getContext('2d');
+
+            const data = {
+                labels: this.labels,
+
+                datasets: [
+                    {
+                        data: this.values
+                    }
+                ]
+            };
+
+            new Chart(canvas, {
+                type: 'line',
+                data: data
+            });
+
+        }
+
+    }
+</script>
