1f03587b5f39e76ab7e0142167ff91ac0b66ae57
fruppel <skywalker9947>
2018-04-26 21:21:58 +0200

    Extend profile page

--- app/User.php
+++ app/User.php
@@ -28,8 +28,34 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'birthdate'
+    ];
+
     public function getAgeAttribute()
     {
         return Carbon::parse($this->birthdate)->diffInYears();
+    }
+
+    /**
+     * Returns the current weight of the user
+     *
+     * @return mixed
+     */
+    public function getCurrentWeightAttribute()
+    {
+        return $this->weights()->latest()->first()->weight;
+    }
+
+    /**
+     * Defines the weights relation of an user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function weights()
+    {
+        return $this->hasMany(Weight::class);
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,11 +8,19 @@
             <table class="table">
                 <tr>
                     <td>Alter:</td>
-                    <td>{{ $user->age }}</td>
+                    <td>{{ $user->age }} <small>({{ $user->birthdate->format('d.m.Y')  }})</small></td>
                 </tr>
                 <tr>
                     <td>Größe:</td>
                     <td>{{ $user->height }} cm</td>
+                </tr>
+                <tr>
+                    <td>Geschlecht:</td>
+                    <td>{{ $user->gender === 'male' ? 'Männlich' : 'Weiblich' }}</td>
+                </tr>
+                <tr>
+                    <td>Gewicht:</td>
+                    <td>{{ $user->currentWeight }} kg</td>
                 </tr>
             </table>
         </div>
